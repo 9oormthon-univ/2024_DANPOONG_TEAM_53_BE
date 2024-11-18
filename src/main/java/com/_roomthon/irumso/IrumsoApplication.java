f946fbb9c284209ace8ca4eedd6bff4073913faf
leechanghwanspace <spacecehh@naver.com>
2024-11-18 16:06:08 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/_roomthon/irumso/IrumsoApplication.java
@@ -0,0 +1,13 @@
+package com._roomthon.irumso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrumsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrumsoApplication.class, args);
+	}
+
+}
